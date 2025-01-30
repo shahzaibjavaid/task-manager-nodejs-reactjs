b0b181fbd6ac59dc94017a103ba2ce1442a99a6d
Shahzaib Javaid <shahzaibjavaid68@gmail.com>
2025-01-30 21:04:21 +0500

    Fix server port

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const startServer = async () => {
     try {
         await getConnection();
         console.log("Database connection established");
-        const PORT = process.env.PORT || 5000;
+        const PORT = process.env.PORT || 8000;
         app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
     } catch (error) {
         console.error("Failed to start server:", error.message);
