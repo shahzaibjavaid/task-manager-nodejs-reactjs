8ff4300286ae3bd143cddaa910178747916e1fda
Shahzaib Javaid <shahzaibjavaid68@gmail.com>
2025-01-30 11:46:56 +0500

    get user api

--- server.js
+++ server.js
@@ -5,6 +5,16 @@ app.get("/api/get", (req, res) => {
     res.send({message: "Hello, listening"})
 })
 
+app.get("/api/get_user", (req, res) => {
+    res.send({
+        user:{
+            name: "Shahzaib",
+            age: 29,
+            contact: 123456789
+        }
+    })
+})
+
 app.listen(8000, () => {
     console.log("listening on 8000")
 })
