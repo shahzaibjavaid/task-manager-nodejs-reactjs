b3678680b6f5c5bca041881066d223fc919b37e8
Shahzaib Javaid <shahzaibjavaid68@gmail.com>
2025-01-30 16:43:38 +0500

    Database fix

--- server.js
+++ server.js
@@ -3,7 +3,21 @@ const app = express()
 require("dotenv").config()
 const mysql = require("mysql")
 
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DATABASE_NAME,
+})
 
+db.connect(err => {
+    if(err){
+        console.log(err.message)
+        return
+    }
+    console.log("Database connected")
+})
 
 app.get("/api/get", (req, res) => {
     res.send({message: "Hello, listening"})
