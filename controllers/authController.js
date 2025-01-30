66c44bde21e1cf538225cb31558d33232998fc14
Shahzaib Javaid <shahzaibjavaid68@gmail.com>
2025-01-30 21:05:04 +0500

    Remove call log

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,9 +3,7 @@ const jwt = require("jsonwebtoken");
 const { getConnection } = require("../config/db");
 
 exports.register = async (req, res) => {
-    console.log('call log register')
     const { username, password } = req.body;
-
     if (!username || !password) {
         return res.status(400).json({ error: "Username and password are required" });
     }
@@ -34,9 +32,7 @@ exports.register = async (req, res) => {
 };
 
 exports.login = async (req, res) => {
-    console.log('call log login')
     const { username, password } = req.body;
-
     if (!username || !password) {
         return res.status(400).json({ error: "Username and password are required" });
     }
