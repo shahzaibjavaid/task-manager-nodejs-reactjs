10f25734aeb4f314a9e41a8c1f09abe1e9a391f9
Shahzaib Javaid <shahzaibjavaid68@gmail.com>
2025-01-30 20:20:28 +0500

    Add cors

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cors = require('cors');
+
 const { getConnection } = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
 const taskRoutes = require("./routes/taskRoutes");
@@ -8,6 +10,7 @@ const { authenticateJWT } = require("./middlewares/authMiddleware");
 dotenv.config();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/v1/auth", authRoutes);
