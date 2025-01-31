40f26c18dd467d17da5d005a28c01f1c54c8c778
Shahzaib Javaid <shahzaibjavaid68@gmail.com>
2025-01-31 11:11:21 +0500

    Vite config fix

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react-swc'
+const oneYearInSeconds = 60 * 60 * 24 * 365
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
+  server: {
+    https: false,
+    headers: {
+      'Strict-Transport-Security': `max-age=${oneYearInSeconds}`,
+    },
+  },
 })
